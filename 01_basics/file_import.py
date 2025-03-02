bf3c149a95311c98b501df64b65b61d3711bd649
md-kamrul <join.kamrul@gmail.com>
2025-03-02 12:06:55 +0600

    priting and importing file learning done

---
+++ 01_basics/file_import.py
@@ -0,0 +1,5 @@
+from hello import my_funt 
+
+my_funt("importing file from hello.py") 
+
+# my_funtion(10) 
---
+++ 01_basics/hello.py
@@ -0,0 +1,8 @@
+print("hello python")   #printing hello python
+
+# defining function
+def my_funt(n):
+    print(n)
+
+# calling function
+my_funt("python function calling") 
